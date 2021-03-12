a872709b39be9f348d70dbe5c1871ea79c13fc28
KseniaW <ksen33333@gmail.com>
2021-03-11 20:56:18 -0500

    lala

--- src/testTheGit/git.java
+++ src/testTheGit/git.java
@@ -3,8 +3,12 @@ package testTheGit;
 public class git {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		System.out.println("Ksenia");
+		System.out.println("Have a great day");
+		System.out.println("Hi");
+		System.out.println("Love");
+		System.out.println("Java");
 	}
 
 }
